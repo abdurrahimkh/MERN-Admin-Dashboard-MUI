c7cc3c506a94dffbd7b2298a3db6458234c06e9e
abdurrahimkh <abdurrahimkhan9574@gmail.com>
2023-01-01 11:59:47 +0500

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,27 @@ import { ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { useMemo } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { themeSettings } from './theme';
+import Dashboard from './pages/dashboard';
+import Layout from './pages/layout';
 const App = () => {
   const mode = useSelector(state => state.global.mode);
   console.log(mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+            <Route path='/' element={<Navigate to='/dashboard' replace />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 };
