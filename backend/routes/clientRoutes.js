da31f47e4a84842d6179f68d9ca082c3ab0a4b88
abdurrahimkh <abdurrahimkhan9574@gmail.com>
2023-01-05 20:53:25 +0500

    Product Model & Route

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,3 +1,7 @@
 import express from 'express';
+import { getProducts } from '../controllers/clientController.js';
 const router = express.Router();
+
+router.get('/products', getProducts);
+
 export default router;
