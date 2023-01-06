27811a5588051a37d1996282916ab624461fe7a0
abdurrahimkh <abdurrahimkhan9574@gmail.com>
2023-01-06 14:23:14 +0500

    Get Customers Api

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { getProducts } from '../controllers/clientController.js';
+import { getProducts, getCustomers } from '../controllers/clientController.js';
 const router = express.Router();
 
 router.get('/products', getProducts);
+router.get('/customers', getCustomers);
 
 export default router;
