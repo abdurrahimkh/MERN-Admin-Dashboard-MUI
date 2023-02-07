e638a381347dda0446b0703fbe8427d4eec59af4
abdurrahimkh <abdurrahimkhan9574@gmail.com>
2023-02-07 10:47:51 +0500

    Geography API

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getProducts, getCustomers, getTransactions } from '../controllers/clientController.js';
+import { getProducts, getCustomers, getTransactions, getGeogrophy } from '../controllers/clientController.js';
 const router = express.Router();
 
 router.get('/products', getProducts);
 router.get('/customers', getCustomers);
 router.get('/transactions', getTransactions);
+router.get('/geography', getGeogrophy);
 
 export default router;
